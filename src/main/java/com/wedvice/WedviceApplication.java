1149e9b4daace849e4f667d40b0cf2e3f711bb67
devkss0312 <devkss0312@gmail.com>
2025-02-19 23:50:44 +0900

    feat: initial spring boot setup

---
+++ src/main/java/com/wedvice/WedviceApplication.java
@@ -0,0 +1,13 @@
+package com.wedvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedvice/WedviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
