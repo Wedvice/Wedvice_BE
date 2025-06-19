105242d855003712c2dcad556cfcfadfd90528d4
cloud <goorm94@naver.com>
2025-06-19 15:30:32 +0900

    feat: jpaAuditing 작성자,수정자 기능 추가

---
+++ src/main/java/com/wedvice/common/config/JpaAuditingConfiguration.java
@@ -0,0 +1,23 @@
+package com.wedvice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfiguration {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(
+//                "hyunggeun"
+                SecurityContextHolder.getContext().getAuthentication().getName()
+
+        );
+    }
+}
--- src/main/java/com/wedvice/task/Task.java
+++ src/main/java/com/wedvice/task/Task.java
@@ -18,5 +18,4 @@ public class Task extends BaseEntity {
 
 
 
-//    s3준비.
 }
