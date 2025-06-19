48d3dd624560641f69da4e6ce37cc9342573147a
cloud <goorm94@naver.com>
2025-06-19 15:40:53 +0900

    feat: cors 도메인 설정 및 환경설정 빈 추가

--- src/main/java/com/wedvice/security/CorsConfig.java
+++ src/main/java/com/wedvice/security/CorsConfig.java
@@ -6,18 +6,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@Configuration
 public class CorsConfig {
 
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("http://localhost:3000") // React 앱 주소
-                        .allowedMethods("*")
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://www.wedy.co.kr")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // 쿠키 포함 허용
+                        .allowCredentials(true);
             }
         };
     }
