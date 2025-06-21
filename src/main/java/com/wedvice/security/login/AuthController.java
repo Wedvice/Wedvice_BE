bd7c75f74362bc7afb1d371ebaed7a37cd83dcda
cloud <goorm94@naver.com>
2025-06-21 16:49:58 +0900

    feat: 로그인 시 유저의 필수정보 입력에 따른 redirection 뷰 지정되는 로직 통합

--- src/main/java/com/wedvice/security/login/AuthController.java
+++ src/main/java/com/wedvice/security/login/AuthController.java
@@ -28,65 +28,13 @@ public class AuthController {
     private final UserRepository userRepository;
 
 
-    //    필요한지?
+    /**
+     1. 로그인 성공 후 백엔드에서 프론트에 /Redirection으로 보낸다
+     2. 프론트에서는 그 후 /status로 요청을 보낸다
+     3. /status경로는 security filter에서 permitall . 로그인이 안되어있으면 isLoggedIn : false로 반환값이 간다.
+     4. 정상이면 여기서 화면 값을 받아간다.
+     **/
     @GetMapping("/status")
-    public String loginStatus() {
-
-        String response = "";
-
-        log.info("======"+SecurityContextHolder.getContext().getAuthentication());
-        log.info("++++"+SecurityContextHolder.getContext());
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        log.info((authentication != null)+"");
-
-        log.info(authentication.isAuthenticated()+"");
-        log.info(authentication.getPrincipal() instanceof CustomUserDetails +"");
-        log.info(authentication.getPrincipal().toString());
-
-        if (authentication != null && authentication.isAuthenticated() &&
-                authentication.getPrincipal() instanceof CustomUserDetails) {
-            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-             response += "일단 로그인 완료" + customUserDetails+"\n";
-        } else {
-            response += "일단 로그인 아님\n";
-        }
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        User user = userRepository.findById(customUserDetails.getUserId()).orElseThrow();
-
-        if (user.getCouple() == null) {
-            return response += "매칭 필요";
-        }
-
-
-        if(user.getNickname() == null){
-
-            return response += " 닉네임 입력필요";
-        }
-
-        if (user.getRole() == null) {
-            return response += "성별입력필요";
-        }
-
-        Optional<User> matchedUser = user.getCouple().getUsers().stream()
-                .filter(anotherUser -> !anotherUser.getId().equals(user.getId()))
-                .findFirst();
-
-        User matchingUser = matchedUser.get();
-
-        if(matchingUser.getRole()!=null && matchingUser.getNickname()!=null){
-
-            return response += "홈 페이지 이동 ";
-
-        }
-
-        return "예시 -> 뭔가 알수없는 에러";
-
-    }
-
-    @GetMapping("/redirect")
     @Operation(
             summary = "로그인 후 리다이렉트 정보 조회",
             description = "현재 본인의 회원가입 진행 상태를 조회합니다."
