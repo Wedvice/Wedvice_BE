451779801255fce111fb6f93ab1d7cf330f026df
jehyuck <wpgur07@gmail.com>
2025-07-09 15:50:32 +0900

    refactor: subTask 리팩토링 - homeSubtask조회 방식, Repository에서 DTO 반환 -> entity반환 후   Service에서 조립 - coupleId조회 방식 변경 -> 다수의 메서드에서 coupleId직접 조회 ->   couplId조회 및 에러처리 공통 메서드 작성 및 서비스에서 에러처리로 변경 - 이에 따른 완료율 조회도 변경 - 완료율 조회 Role파라미터 추가 -> 함께, 신랑, 신부, 전부 조회 가능한   방법으로 api 수정

--- src/main/java/com/wedvice/subtask/service/SubTaskService.java
+++ src/main/java/com/wedvice/subtask/service/SubTaskService.java
@@ -1,19 +1,19 @@
 package com.wedvice.subtask.service;
 
-import com.wedvice.coupletask.entity.CoupleTask;
 import com.wedvice.subtask.dto.CompleteRateResponseDto;
+import com.wedvice.subtask.dto.HomeSubTaskConditionDto;
 import com.wedvice.subtask.dto.SubTaskHomeResponseDto;
 import com.wedvice.subtask.dto.SubTaskResponseDTO;
 import com.wedvice.subtask.entity.SubTask;
 import com.wedvice.subtask.exception.NotExistRoleException;
 import com.wedvice.subtask.repository.SubTaskRepository;
-import com.wedvice.task.entity.Task;
 import com.wedvice.user.entity.User;
-import java.time.LocalDate;
+import com.wedvice.user.repository.UserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,8 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SubTaskService {
 
-  private final SubTaskRepository subTaskRepository;
-
+    private final SubTaskRepository subTaskRepository;
+    private final UserRepository userRepository;
 
   public List<SubTaskResponseDTO> getAllSubTask(Long userId, Long taskId) {
 
@@ -31,27 +31,65 @@ public class SubTaskService {
   }
 
 
-  @Transactional(readOnly = true)
-  public Slice<SubTaskHomeResponseDto> getHomeSubTasks(Long userId, Boolean completed, String role,
-      Pageable pageable, boolean top3, String sort) {
-    User.Role roleEnum = convertToRole(role); // 👈 문자열 → enum
+    @Transactional(readOnly = true)
+    public Slice<SubTaskHomeResponseDto> getHomeSubTasks(Long userId, Boolean completed,
+        String roleStr, Pageable pageable, boolean top3, String sort) {
+        User.Role role = convertToRole(roleStr);
 
-    return subTaskRepository.findHomeSubTasksByCondition(userId, completed, top3, roleEnum, sort,
-        pageable);
-  }
+        Long coupleId = userRepository.findCoupleIdByUserId(userId);
+        if (coupleId == null) {
+            return new SliceImpl<>(List.of());
+        }
 
-  private User.Role convertToRole(String roleStr) {
-      if (roleStr == null || roleStr.isBlank()) {
-          return null;
-      }
-    try {
-      return User.Role.valueOf(roleStr.toUpperCase());
-    } catch (IllegalArgumentException e) {
-      throw new NotExistRoleException();
+        HomeSubTaskConditionDto homeSubTaskConditionDto = HomeSubTaskConditionDto.builder()
+            .coupleId(coupleId)
+            .completed(completed)
+            .pageable(pageable)
+            .role(role)
+            .sort(sort)
+            .top3(top3)
+            .build();
+        List<SubTask> tasks = subTaskRepository.findHomeSubTasksByCondition(
+            homeSubTaskConditionDto);
+        List<SubTaskHomeResponseDto> dtos = tasks.stream()
+            .map(st -> SubTaskHomeResponseDto.builder()
+                .subTaskId(st.getId())
+                .coupleTaskId(st.getCoupleTask().getId())
+                .subTaskContent(st.getContent())
+                .taskContent(st.getCoupleTask().getTask().getTitle())
+                .targetDate(st.getTargetDate())
+                .completed(st.getCompleted())
+                .orders(st.getOrders())
+                .build())
+            .toList();
+
+        boolean hasNext = !top3 && dtos.size() > pageable.getPageSize();
+        if (hasNext) {
+            dtos.remove(dtos.size() - 1);
+        }
+        return new SliceImpl<>(dtos, pageable, hasNext);
     }
-  }
 
-  public CompleteRateResponseDto getProgressRate(Long userId) {
-    return subTaskRepository.getProgressRate(userId);
-  }
+    private User.Role convertToRole(String roleStr) {
+        if (roleStr == null || roleStr.isBlank()) {
+            return null;
+        }
+        try {
+            return User.Role.valueOf(roleStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new NotExistRoleException();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public CompleteRateResponseDto getProgressRate(Long userId, String role) {
+        Long coupleId = userRepository.findCoupleIdByUserId(userId);
+
+        if (coupleId == null) {
+            return new CompleteRateResponseDto(0, 0, 0);
+        }
+
+        User.Role filterRole = convertToRole(role);
+        return subTaskRepository.getProgressRate(userId, filterRole);
+    }
 }
