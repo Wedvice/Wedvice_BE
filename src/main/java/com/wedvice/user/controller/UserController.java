afe9b7bc09db67eeeef1b287fef213a97ce2f65f
cloud <goorm94@naver.com>
2025-07-18 23:45:20 +0900

    feat: mypage 기본 api 엔드포인트 설정

--- src/main/java/com/wedvice/user/controller/UserController.java
+++ src/main/java/com/wedvice/user/controller/UserController.java
@@ -20,9 +20,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -118,5 +120,135 @@ public class UserController {
         @LoginUser CustomUserDetails loginUser) {
         MyPageMainResponseDto responseDto = userService.getMyPageInfo(loginUser.getUserId());
         return ResponseEntity.ok(ApiResponse.success(responseDto));
+    }
+
+    @Operation(
+        summary = "마이페이지 사진(이미지) 삭제 -> 기본 이미지로 변경",
+        description = "로그인한 사용자의 설정된 사진 삭제",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @DeleteMapping("/image")
+    public void deleteProfileImage(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "마이페이지 사진(이미지) 수정",
+        description = "로그인한 사용자의 설정된 사진 수정",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @PostMapping("/image")
+    public void changeProfileImage(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "파트너 이미지 조회",
+        description = "로그인한 사용자의 설정된 파트너 사진 및 파트너 컬러 조회",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/partnerImage")
+    public void getPartnerImage(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "파트너 연결 끊기 ",
+        description = "로그인한 사용자의 설정된 파트너 연결 끊기",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @DeleteMapping("/partner")
+    public void deletePartner(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "마이페이지 계정관리 ",
+        description = "자신의 닉네임,이메일,프로필 이미지 조회",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/myAccount")
+    public void getMyAccountInfo(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "로그아웃 ",
+        description = "자신 로그아웃",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/myAccount")
+    public void logout(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "계정삭제 ",
+        description = "자신 계정삭제",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @DeleteMapping("/myAccount")
+    public void deleteMyAccount(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "닉네임변경 ",
+        description = "자신 닉네임변경",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @PatchMapping("/myAccount")
+    public void updateNickName(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "컬러 설정조회 ",
+        description = "자신과파트너 우리 개인이 설정한 컬러 설정 조회",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/colorConfig")
+    public void getColorConfig(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "컬러 설정 업데이트 ",
+        description = "자신과 파트너의 개인 컬러 설정 업데이트",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/colorConfig")
+    public void updateColorConfig(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "결혼 예정일 조회 ",
+        description = "결혼 예정일 조회",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @GetMapping("/weddingDate")
+    public void getWeddingDate(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "결혼 예정일 수정 ",
+        description = "결혼 예정일 수정",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @PatchMapping("/weddingDate")
+    public void updateWeddingDate(@LoginUser CustomUserDetails loginUser) {
+
+    }
+
+    @Operation(
+        summary = "알림 설정 조회 ",
+        description = "알림 설정 조회",
+        security = @SecurityRequirement(name = JWT)
+    )
+    @PatchMapping("/weddingDate")
+    public void getAlarmConfig(@LoginUser CustomUserDetails loginUser) {
+
     }
 }
--- src/main/java/com/wedvice/user/entity/UserConfig.java
+++ src/main/java/com/wedvice/user/entity/UserConfig.java
@@ -1,10 +1,11 @@
 package com.wedvice.user.entity;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,9 +28,13 @@ public class UserConfig {
     @Column(name = "user_config_id")
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToOne(fetch = LAZY, mappedBy = "userConfig")
+    private UserAlarmConfig alarmConfig;
+
 
     @Getter
     private LocalDateTime weddingDate;
