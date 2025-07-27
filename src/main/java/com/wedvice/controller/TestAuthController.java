37f1a6c3a71a84b6c6cf2385d640d794d7a69cd9
jehyuck <wpgur07@gmail.com>
2025-07-27 17:29:52 +0900

    feat: test관련 코드 배포에도 넣기

--- src/main/java/com/wedvice/controller/TestAuthController.java
+++ src/main/java/com/wedvice/controller/TestAuthController.java
@@ -16,7 +16,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("local") // 또는 직접 env 체크
+//@Profile("local") // 또는 직접 env 체크
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/test-auth")
